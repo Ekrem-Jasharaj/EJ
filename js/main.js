ebae5977551be8da565fa60d04110612df913482
Ekrem-Jasharaj <147052487+Ekrem-Jasharaj@users.noreply.github.com>
2023-10-10 14:44:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,14 +10,12 @@ function includeHTML() {
         if (this.readyState == 4) {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
       }      
       xhttp.open("GET", file, true);
       xhttp.send();
-      /*exit the function:*/
       return;
     }
   }
