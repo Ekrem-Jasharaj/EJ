83305376ffb9dc6e23b4b08dfe78c1546dc49166
Ekrem-Jasharaj <147052487+Ekrem-Jasharaj@users.noreply.github.com>
2023-10-10 12:42:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 alert("github");
+console.log("aaa);
 
-function  toggleClass(x,y) {
-    document.getElementById(x).classList.toggle(y); 
-}
+//function  toggleClass(x,y) {
+   // document.getElementById(x).classList.toggle(y); 
+//}
